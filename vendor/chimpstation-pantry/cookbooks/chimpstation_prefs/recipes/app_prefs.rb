15f8b86ce4feca69bcb3d834fda876a8f03714b0
Philip (flip) Kromer <flip@infochimps.org>
2012-03-08 04:17:15 -0600

    fixed up defaults writing

--- vendor/chimpstation-pantry/cookbooks/chimpstation_prefs/recipes/app_prefs.rb
+++ vendor/chimpstation-pantry/cookbooks/chimpstation_prefs/recipes/app_prefs.rb
@@ -5,75 +5,61 @@
 #
 
 domain = "com.apple.finder"
-osx_defaults(domain, "_FXShowPosixPathInTitle"){ value node[:osx_prefs][:finder][:show_full_path] }
-osx_defaults(domain, "ShowHardDrivesOnDesktop"){ value node[:osx_prefs][:finder][:show_hard_drives] }
-
-# osx_defaults(domain, "AppleShowAllFiles"      ){ value node[:osx_prefs][:finder][:show_all_files] }
-# osx_defaults(domain, "ShowStatusBar"){ value node[:osx_prefs][:finder][:show_hard_drives] }
-# osx_defaults(domain, "ShowHardDrivesOnDesktop"){ value node[:osx_prefs][:finder][:show_hard_drives] }
-
-# # Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons
-# osx_defaults('com.apple.finder',                      "QuitMenuItem"	){ value 'true' }
-
-# Show all filename extensions in Finder
-osx_defaults('NSGlobalDomain',                          "AppleShowAllExtensions"	){ value 'true' }
-
-# Expand save panel by default
-osx_defaults('NSGlobalDomain',                          "NSNavPanelExpandedStateForSaveMode"	){ value 'true' }
-
-# Expand print panel by default
-osx_defaults('NSGlobalDomain',                          "PMPrintingExpandedStateForPrint"	){ value 'true' }
-
-# # Disable the “Are you sure you want to open this application?” dialog
-# osx_defaults('com.apple.LaunchServices',              "LSQuarantine"	){ value 'false' }
-
-# # Disable shadow in screenshots
-# osx_defaults('com.apple.screencapture',               "disable-shadow"	){ value 'true' }
-
-# Avoid creating .DS_Store files on network volumes
-osx_defaults('com.apple.desktopservices',                            	"DSDontWriteNetworkStores"	){ value 'true' }
-
-# # Disable the warning when changing a file extension
-# osx_defaults('com.apple.finder',                                   	"FXEnableExtensionChangeWarning"	){ value 'false' }
-
-# Show item info below desktop icons
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
-
-# # Disable the warning before emptying the Trash
-# osx_defaults('com.apple.finder',                                   	"WarnOnEmptyTrash"	){ value 'false' }
-
-# # Empty Trash securely by default
-# osx_defaults('com.apple.finder',                                   	"EmptyTrashSecurely"	){ value 'true' }
 
 # Display full POSIX path as Finder window title
-osx_defaults('com.apple.finder',                                     	"_FXShowPosixPathInTitle"	){ value 'true' }
+osx_defaults(domain, "_FXShowPosixPathInTitle"   ){ value node[:osx_prefs][:finder][:show_full_path] }
+osx_defaults(domain, "ShowHardDrivesOnDesktop"   ){ value node[:osx_prefs][:finder][:show_hard_drives] }
+# osx_defaults(domain, "AppleShowAllFiles"       ){ value node[:osx_prefs][:finder][:show_all_files] }
+# osx_defaults(domain, "ShowStatusBar"           ){ value node[:osx_prefs][:finder][:show_status_bar] }
+# osx_defaults(domain, "ShowHardDrivesOnDesktop" ){ value node[:osx_prefs][:finder][:show_hard_drives] }
 
-# Disable disk image verification
-osx_defaults('com.apple.frameworks.diskimages',                      	"skip-verify"	){ value 'true' }
-osx_defaults('com.apple.frameworks.diskimages',                      	"skip-verify-locked"	){ value 'true' }
-osx_defaults('com.apple.frameworks.diskimages',                      	"skip-verify-remote"	){ value 'true' }
+#
+# Global Domain
+#
 
+domain = 'NSGlobalDomain'
+
+# Show all filename extensions in Finder
+osx_defaults(domain, "AppleShowAllExtensions"             ){ value true }
+# Expand save panel by default
+osx_defaults(domain, "NSNavPanelExpandedStateForSaveMode" ){ value true }
+# Expand print panel by default
+osx_defaults(domain, "PMPrintingExpandedStateForPrint"    ){ value true }
+# Don't re-open all my windows every time I restart an app
+osx_defaults(domain, "NSQuitAlwaysKeepsWindows"           ){ value false }
+
+#
+# other
+#
+
+# # The “Are you sure you want to open this application?” dialog
+osx_defaults('com.apple.LaunchServices', "LSQuarantine"	){ value true }
+# Avoid creating .DS_Store files on network volumes
+osx_defaults('com.apple.desktopservices',  "DSDontWriteNetworkStores"	){ value true }
 # Automatically open a new Finder window when a volume is mounted
-osx_defaults('com.apple.frameworks.diskimages',                      	"auto-open-ro-root"	){ value 'true' }
-osx_defaults('com.apple.frameworks.diskimages',                      	"auto-open-rw-root"	){ value 'true' }
+osx_defaults('com.apple.frameworks.diskimages', "auto-open-ro-root"	){ value true }
+osx_defaults('com.apple.frameworks.diskimages', "auto-open-rw-root"	){ value true }
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+osx_defaults('com.apple.NetworkBrowser',    "BrowseAllInterfaces"   ){ value true }
+# Show remaining battery time; hide percentage
+osx_defaults('com.apple.menuextra.battery', "ShowPercent"   ){ value false }
+osx_defaults('com.apple.menuextra.battery', "ShowTime"      ){ value true }
 
-# # Disable Resume system-wide
-# osx_defaults('NSGlobalDomain',                                     	"NSQuitAlwaysKeepsWindows"	){ value 'false' }
+#
+# FIXME: these need sudo
+#
 
-# # Enable Dashboard dev mode (allows keeping widgets on the desktop)
-# osx_defaults('com.apple.dashboard',                                	"devmode"	){ value 'true' }
-
-# System Info, Hostname & Version on Login Screen (must click on Clock to see hostname & version info)
-osx_defaults('/Library/Preferences/com.apple.loginwindow',           	"AdminHostInfo"	){ value 'HostName' }
-
-# Enable logon to Novell & other Pre Lion Mac-OS-X NAS-shares (requires restart)
-# after restart, connect to any old AFP server (use "Connect To...") so it creates some magic files
-osx_defaults('/Library/Preferences/com.apple.AppleShareClient',      	"afp_host_prefs_version"	){ value '1' }
-
-# # Show the ~/Library folder
+# # System Info, Hostname & Version on Login Screen (must click on Clock to see hostname & version info)
+# osx_defaults('/Library/Preferences/com.apple.loginwindow', "AdminHostInfo" ){ value 'HostName' }
+# # Enable logon to Novell & other Pre Lion Mac-OS-X NAS-shares (requires restart)
+# # Note: after you restart, connect to an AFP server -- it will create some magic files that complete the setup.
+# osx_defaults('/Library/Preferences/com.apple.AppleShareClient', "afp_host_prefs_version" ){ value 1 }
+#
+# Show item info below desktop icons :TODO:
+# /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+# # Show the ~/Library folder :TODO:
 # chflags nohidden ~/Library
-
-# # Disable local Time Machine backups
+# # Disable local Time Machine backups :TODO:
 # sudo tmutil enablelocal
 
 #
@@ -89,54 +75,35 @@ end
 # src=$HOME/Library/Preferences/com.apple.Terminal.plist ; dest=$HOME/ics/sysadmin/chimpstation/cookbooks/chimpstation_prefs/templates/default/com.apple.Terminal.plist.erb  ; cp $src $dest ; plutil -convert xml1 $dest
 
 domain = "com.apple.Terminal"
+# Window theme
 osx_defaults(domain, "Default Window Settings"){ value node[:osx_prefs][:terminal][:theme] }
 osx_defaults(domain, "Startup Window Settings"){ value node[:osx_prefs][:terminal][:theme] }
-
-# # Only use UTF-8 in Terminal.app
-# osx_defaults('com.apple.terminal',                    "StringEncodings"	){ value '4' }
+# Only use UTF-8 in Terminal.app
+osx_defaults(domain, "StringEncodings"        ){ value 4 }
 
 #
 # Safari
 #
 
 domain = "com.apple.safari"
-osx_defaults(domain, "AutoFillPasswords"             ){ value node[:osx_prefs][:safari][:auto_fill_passwords] }
-osx_defaults(domain, "HistoryAgeInDaysLimit"         ){ value node[:osx_prefs][:safari][:history_limit] }
+osx_defaults(domain, "AutoFillPasswords"              ){ value node[:osx_prefs][:safari][:auto_fill_passwords] }
+osx_defaults(domain, "HistoryAgeInDaysLimit"          ){ value node[:osx_prefs][:safari][:history_limit] }
 
-osx_defaults(domain, "WarnAboutFraudulentWebsites"   ){ value true }
-osx_defaults(domain, "WebKitTabToLinksPreferenceKey" ){ value true }
-osx_defaults(domain, "WebKitFixedFont"               ){ value 'Menlo' }
+osx_defaults(domain, "WarnAboutFraudulentWebsites"    ){ value true }
+osx_defaults(domain, "WebKitTabToLinksPreferenceKey"  ){ value true }
+osx_defaults(domain, "WebKitFixedFont"                ){ value 'Menlo' }
 
-osx_defaults(domain, "IncludeDebugMenu"              ){ value node[:osx_prefs][:safari][:show_debug_menu]    } 
-osx_defaults(domain, "IncludeDevelopMenu"            ){ value node[:osx_prefs][:safari][:show_develop_menu]  }
-osx_defaults(domain, "ShowFavoritesBar"              ){ value node[:osx_prefs][:safari][:show_favorites_bar] }
-osx_defaults(domain, "ShowStatusBar"                 ){ value node[:osx_prefs][:safari][:show_status_bar]    }
-osx_defaults(domain, "AlwaysShowTabBar"              ){ value node[:osx_prefs][:safari][:show_tab_bar]       }
-
-# # Disable Safari’s thumbnail cache for History and Top Sites
-# osx_defaults('com.apple.Safari',                      "DebugSnapshotsUpdatePolicy"	){ value '2' }
-
-# Enable Safari’s debug menu
-osx_defaults('com.apple.Safari',                        "IncludeDebugMenu"	){ value 'true' }
-
-# # Remove useless icons from Safari’s bookmarks bar
-# defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+osx_defaults(domain, "IncludeDebugMenu"               ){ value node[:osx_prefs][:safari][:show_debug_menu]    } 
+osx_defaults(domain, "IncludeDevelopMenu"             ){ value node[:osx_prefs][:safari][:show_develop_menu]  }
+osx_defaults(domain, "ShowFavoritesBar"               ){ value node[:osx_prefs][:safari][:show_favorites_bar] }
+osx_defaults(domain, "ShowStatusBar"                  ){ value node[:osx_prefs][:safari][:show_status_bar]    }
+osx_defaults(domain, "AlwaysShowTabBar"               ){ value node[:osx_prefs][:safari][:show_tab_bar]       }
 
 #
 # iTunes
 #
 
 # Disable the Ping sidebar in iTunes
-osx_defaults('com.apple.iTunes',                        "disablePingSidebar"	){ value 'true' }
-
+osx_defaults('com.apple.iTunes', "disablePingSidebar" ){ value true }
 # Disable all the other Ping stuff in iTunes
-osx_defaults('com.apple.iTunes',                        "disablePing"	){ value 'true' }
-
-#
-# Mail
-#
-
-# Disable send and reply animations in Mail.app
-osx_defaults('com.apple.Mail',                          "DisableReplyAnimations"	){ value 'true' }
-osx_defaults('com.apple.Mail',                          "DisableSendAnimations"	){ value 'true' }
-
+osx_defaults('com.apple.iTunes', "disablePing"        ){ value true }
--- vendor/chimpstation-pantry/cookbooks/chimpstation_prefs/recipes/user_interface_prefs.rb
+++ vendor/chimpstation-pantry/cookbooks/chimpstation_prefs/recipes/user_interface_prefs.rb
@@ -4,21 +4,17 @@
 # Keyboard
 #
 
-domain = "~/Library/Preferences/.GlobalPreferences"
-osx_defaults(domain, 'KeyRepeat'       ){ value node[:osx_prefs][:keyboard][:key_repeat_delay] }
-osx_defaults(domain, 'InitialKeyRepeat'){ value node[:osx_prefs][:keyboard][:initial_repeat_delay] }
-
-# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
-osx_defaults('NSGlobalDomain', "AppleKeyboardUIMode" ){ value 3 }
-
+domain = '~/Library/Preferences/.GlobalPreferences'
+# Set keyboard repeat rate
+osx_defaults(domain, "KeyRepeat"                ){ value node[:osx_prefs][:keyboard][:key_repeat_delay] }
+osx_defaults(domain, "InitialKeyRepeat"         ){ value node[:osx_prefs][:keyboard][:initial_repeat_delay] }
 # Disable press-and-hold for keys in favor of key repeat
-osx_defaults('NSGlobalDomain', "ApplePressAndHoldEnabled"      ){ value 'false' }
+osx_defaults(domain, "ApplePressAndHoldEnabled" ){ value false }
 
-# Set a blazingly fast keyboard repeat rate
-osx_defaults('NSGlobalDomain', "KeyRepeat"     ){ value '0' }
-
+# Enable full keyboard access for all controls, eg enable Tab in modal dialogs
+osx_defaults(domain, "AppleKeyboardUIMode"      ){ value 3 }
 # Disable auto-correct
-osx_defaults('NSGlobalDomain', "NSAutomaticSpellingCorrectionEnabled"  ){ value 'false' }
+osx_defaults(domain, "NSAutomaticSpellingCorrectionEnabled" ){ value false }
 
 
 #
@@ -26,11 +22,11 @@ osx_defaults('NSGlobalDomain', "NSAutomaticSpellingCorrectionEnabled"  ){ value 
 #
 
 domain = 'com.apple.driver.AppleBluetoothMultitouch.trackpad'
-osx_defaults(domain, 'Clicking' ){ value node[:osx_prefs][:trackpad][:clicking_by_touch] }
-osx_defaults(domain, 'Dragging' ){ value node[:osx_prefs][:trackpad][:dragging_by_touch] }
+osx_defaults(domain, "Clicking" ){ value node[:osx_prefs][:trackpad][:clicking_by_touch] }
+osx_defaults(domain, "Dragging" ){ value node[:osx_prefs][:trackpad][:dragging_by_touch] }
 # Map bottom right Trackpad corner to right-click
-osx_defaults(domain,    "TrackpadCornerSecondaryClick"  ){ value '2' }
-osx_defaults(domain,    "TrackpadRightClick"            ){ value 'true' }
+osx_defaults(domain, "TrackpadCornerSecondaryClick"  ){ value 2 }
+osx_defaults(domain, "TrackpadRightClick"            ){ value true }
 
 #
 # Dock
@@ -39,32 +35,23 @@ osx_defaults(domain,    "TrackpadRightClick"            ){ value 'true' }
 dock = service("com.apple.Dock.agent"){ service_name "com.apple.Dock" }
 domain = 'com.apple.dock'
 
-osx_defaults(domain, "no-glass"    ){ value node[:osx_prefs][:dock][:no_glass]    ; notifies(:restart, dock, :delayed) }
-osx_defaults(domain, "orientation" ){ value node[:osx_prefs][:dock][:orientation] ; notifies(:restart, dock, :delayed) }
-osx_defaults(domain, "autohide"    ){ value node[:osx_prefs][:dock][:autohide]    ; notifies(:restart, dock, :delayed) }
-
-
 # Enable the 2D Dock
-osx_defaults('com.apple.dock',                          "no-glass"      ){ value 'true' }
-
+osx_defaults(domain, "no-glass"                ){ value node[:osx_prefs][:dock][:no_glass]    ; notifies(:restart, dock, :delayed) }
 # Automatically hide and show the Dock
-osx_defaults('com.apple.dock',                          "autohide"      ){ value 'true' }
+osx_defaults(domain, "autohide"                ){ value node[:osx_prefs][:dock][:autohide]    ; notifies(:restart, dock, :delayed) }
+# Which side of the screen
+osx_defaults(domain, "orientation"             ){ value node[:osx_prefs][:dock][:orientation] ; notifies(:restart, dock, :delayed) }
 
 # Make Dock icons of hidden applications translucent
-osx_defaults('com.apple.dock',                          "showhidden"    ){ value 'true' }
-
-# Enable highlight hover effect for the grid view of a stack (Dock)
-osx_defaults('com.apple.dock',                          "mouse-over-hilte-stack"        ){ value 'true' }
-
-# Enable spring loading for all Dock items
-osx_defaults('com.apple.doc',                           "enable-spring-load-actions-on-all-items"       ){ value 'true' }
-
+osx_defaults(domain, "showhidden"              ){ value true ; notifies(:restart, dock, :delayed) }
 # Show indicator lights for open applications in the Dock
-osx_defaults('com.apple.dock',                          "show-process-indicators"       ){ value 'true' }
-
+osx_defaults(domain, "show-process-indicators" ){ value true ; notifies(:restart, dock, :delayed) }
 # Don’t animate opening applications from the Dock
-osx_defaults('com.apple.dock',                          "launchanim"    ){ value 'false' }
-
+osx_defaults(domain, "launchanim"              ){ value false ; notifies(:restart, dock, :delayed) }
+# Enable highlight hover effect for the grid view of a Dock stack
+osx_defaults(domain, "mouse-over-hilte-stack"  ){ value true ; notifies(:restart, dock, :delayed) }
+# Enable spring loading for all Dock items
+osx_defaults(domain, "enable-spring-load-actions-on-all-items" ){ value true ; notifies(:restart, dock, :delayed) }
 
 #
 # Hot Corners
@@ -99,25 +86,13 @@ end
 # UI Display Preferences
 #
 
+domain = '~/Library/Preferences/.GlobalPreferences'
 
 # Enable subpixel font rendering on non-Apple LCDs
-osx_defaults('NSGlobalDomain',                          "AppleFontSmoothing"    ){ value '2' }
-
+osx_defaults(domain,   "AppleFontSmoothing"                 ){ value 2 }
 # Disable menu bar transparency
-osx_defaults('NSGlobalDomain',                          "AppleEnableMenuBarTransparency"        ){ value 'false' }
-
-# Show remaining battery time; hide percentage
-osx_defaults('com.apple.menuextra.battery',             "ShowPercent"   ){ value '"NO"' }
-osx_defaults('com.apple.menuextra.battery',             "ShowTime"      ){ value '"YES"' }
-
+osx_defaults(domain,   "AppleEnableMenuBarTransparency"     ){ value false }
 # Always show scrollbars
-osx_defaults('NSGlobalDomain',                          "AppleShowScrollBars"   ){ value '"Always"' }
-
-# # Disable window animations
-# osx_defaults('NSGlobalDomain',                        "NSAutomaticWindowAnimationsEnabled"    ){ value 'false' }
-
-# Enable AirDrop over Ethernet and on unsupported Macs running Lion
-osx_defaults('com.apple.NetworkBrowser',                "BrowseAllInterfaces"   ){ value 'true' }
-
+osx_defaults(domain,   "AppleShowScrollBars"                ){ value 'Always' }
 # Increase window resize speed for Cocoa applications
-osx_defaults('NSGlobalDomain',                          "NSWindowResizeTime"    ){ value '0.001' }
+osx_defaults(domain,   "NSWindowResizeTime"                 ){ value 0.001 }
